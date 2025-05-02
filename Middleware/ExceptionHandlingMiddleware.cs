ed1285fc0db27100079056491c3f4831619efbcf
dramlian <jankovdamian@gmail.com>
2025-05-02 23:18:07 +0200

    Warning fix

--- Middleware/ExceptionHandlingMiddleware.cs
+++ Middleware/ExceptionHandlingMiddleware.cs
@@ -23,7 +23,7 @@ public class ExceptionHandlingMiddleware
             context.Response.StatusCode = StatusCodes.Status400BadRequest;
             await context.Response.WriteAsJsonAsync(new { error = ex.Message });
         }
-        catch (Exception ex)
+        catch (Exception)
         {
             context.Response.StatusCode = StatusCodes.Status500InternalServerError;
             await context.Response.WriteAsJsonAsync(new { error = "Internal Server Error" });
