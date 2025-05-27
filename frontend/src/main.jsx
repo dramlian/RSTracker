1372bf5b969752c714baa4b8664c93cd66fd11ac
dramlian <jankovdamian@gmail.com>
2025-05-27 22:18:52 +0200

    JWT done and working!

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import { StrictMode, useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard/Dashboard";
@@ -9,26 +9,60 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { ToastContainer } from "react-toastify";
 
 import { PublicClientApplication } from "@azure/msal-browser";
-import { MsalProvider } from "@azure/msal-react";
+import { MsalProvider, useMsal } from "@azure/msal-react";
 import { msalConfig } from "./Authtentification/authConfig";
-import { TokenLogger } from "./Authtentification/TokenLogger";
+
+import api from "./Helpers/ApiClient";
 
 const msalInstance = new PublicClientApplication(msalConfig);
+
+function AppRoutes() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const { accounts, instance } = useMsal();
+
+  useEffect(() => {
+    if (accounts.length > 0) {
+      instance
+        .acquireTokenSilent({
+          account: accounts[0],
+          scopes: ["api://8d32374c-18a3-4253-aed6-9f42d981a68c/access_as_user"],
+        })
+        .then((response) => {
+          api.setToken(response.accessToken);
+          setIsLoggedIn(true);
+        })
+        .catch(() => {
+          api.setToken(null);
+          setIsLoggedIn(false);
+        });
+    } else {
+      api.setToken(null);
+      setIsLoggedIn(false);
+    }
+  }, [accounts, instance]);
+
+  return (
+    <Routes>
+      <Route path="*" element={<Homepage />} />
+      {isLoggedIn && (
+        <>
+          <Route path="/welness" element={<Dashboard type={"welness"} />} />
+          <Route path="/rpe" element={<Dashboard type={"rpe"} />} />
+          <Route path="/management" element={<Management />} />
+        </>
+      )}
+    </Routes>
+  );
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <MsalProvider instance={msalInstance}>
       <BrowserRouter>
         <Header />
-        <Routes>
-          <Route path="/welness" element={<Dashboard type={"welness"} />} />
-          <Route path="/rpe" element={<Dashboard type={"rpe"} />} />
-          <Route path="/management" element={<Management />} />
-          <Route path="*" element={<Homepage />} />
-        </Routes>
+        <AppRoutes />
       </BrowserRouter>
       <ToastContainer />
-      <TokenLogger />
     </MsalProvider>
   </StrictMode>
 );
