6e5cef9a5d65156c5b7f2ffe2724b587a7673ce6
dramlian <jankovdamian@gmail.com>
2025-05-31 18:01:38 +0200

    Just a rename, here the playerhelper will be tested

---
+++ backend/RSTracker.Tests/PlayerHelperTests.cs
@@ -0,0 +1,13 @@
+using RSTracker.Helpers;
+namespace RSTracker.Tests;
+
+public class PlayerHelperTests
+{
+    [Fact]
+    public void Test1()
+    {
+        int a = 2 + 2;
+        Assert.Equal(4, a);
+    }
+
+}
